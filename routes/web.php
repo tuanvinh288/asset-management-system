13b476316e46ef2e009d862f497ec3acee14773d
tuanvinh288 <tuanvinh288@gmail.com>
2025-05-25 23:52:37 +0700

    sửa lỗi route trang đăng nhập

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ use App\Http\Controllers\admin\RoomBorrowController;
 use App\Http\Controllers\admin\RoleController;
 use App\Http\Controllers\admin\SettingController;
 
+// Redirect root to login page
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
